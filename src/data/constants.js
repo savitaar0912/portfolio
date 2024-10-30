ef637d2d30a893f81a696298e92d0a67a93ef267
Shashank Singh <singhshashank875@gmail.com>
2024-10-30 16:41:12 +0530

    youtube link changed

--- src/data/constants.js
+++ src/data/constants.js
@@ -14,7 +14,7 @@ export const Bio = {
   twitter: "https://twitter.com/savitaar875",
   insta: "https://www.instagram.com/_savitaar_/",
   facebook: "https://www.facebook.com/profile.php?viewas=100000686899395&id=100005332446091",
-  youtube: "yt.openinapp.co/lhhz4",
+  youtube: "https://yt.openinapp.co/lhhz4",
 };
 
 export const skills = [
