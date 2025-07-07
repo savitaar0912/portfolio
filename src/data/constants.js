f1e9c3f7760048610e21d2a59c18d39de12b88d4
Shashank Singh <113283329+savitaar0912@users.noreply.github.com>
2025-07-07 21:01:09 +0530

    Update constants.js Resume Updated

--- src/data/constants.js
+++ src/data/constants.js
@@ -7,9 +7,9 @@ export const Bio = {
     "Youtuber",
   ],
   description:
-    "With over 3 years of IT experience in Full Stack Development, Testing, Documentation, Implementation, and Maintenance of applications, I am a dedicated and innovative Full Stack Developer specializing in the MERN stack. Skilled in crafting dynamic and responsive User Interfaces (UI) and enhancing User Experiences (UX) using ReactJS, I have a proven track record of delivering high-quality code and contributing positively to project success. Currently expanding my backend expertise with Node.js, Express.js, and MongoDB to build comprehensive, scalable applications. I am committed to continuous learning and professional growth, actively seeking opportunities to apply my ReactJS skills while deepening my proficiency in full-stack development.",
+    "With over 4 years of IT experience in Full Stack Development, Testing, Documentation, Implementation, and Maintenance of applications, I am a dedicated and innovative Full Stack Developer specializing in the MERN stack. Skilled in crafting dynamic and responsive User Interfaces (UI) and enhancing User Experiences (UX) using ReactJS, I have a proven track record of delivering high-quality code and contributing positively to project success. Currently expanding my backend expertise with Node.js, Express.js, and MongoDB to build comprehensive, scalable applications. I am committed to continuous learning and professional growth, actively seeking opportunities to apply my ReactJS skills while deepening my proficiency in full-stack development.",
   github: "https://github.com/savitaar0912",
-  resume: "https://drive.google.com/file/d/1D2lr4dL_48R0iw3-RXMGOEvMnr-Rh0u5/view?usp=share_link",
+  resume: "https://drive.google.com/drive/folders/1CIuoHteugsp2G9uce4qK_o0OYdono0wW?usp=drive_link",
   linkedin: `https://www.linkedin.com/in/shashanksingh875/`,
   twitter: "https://twitter.com/savitaar875",
   insta: "https://www.instagram.com/codewithsavitaar/",
